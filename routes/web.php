49fbef34b995043926a514c90fab3d068384df01
Quidney <quidney0@gmail.com>
2026-03-09 11:32:48 +0100

    Add Welcome page, add route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Inertia\Inertia;
 
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -10,5 +11,7 @@ Route::inertia('/', 'welcome', [
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
+
+// Route::get('/', function () { return Inertia::render('Welcome'); });
 
 require __DIR__.'/settings.php';
