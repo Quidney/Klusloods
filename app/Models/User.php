b62809a441c3a16013eeb2ee02b354890eb719a2
Zippy6872 <159549946+Zippy6872@users.noreply.github.com>
2026-03-18 11:18:07 +0100

    Revert "27 make models (#31)" (#32)
    This reverts commit 83feb1308f4a52b94aef4fa99791cc70a917b781.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -49,13 +48,5 @@ class User extends Authenticatable
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
-    }
-    public function reservations() :HasMany
-    {
-        return $this->hasMany(Reservation::class);
-    }
-    public function invoices() :HasMany
-    {
-        return $this->hasMany(Invoice::class);
     }
 }
